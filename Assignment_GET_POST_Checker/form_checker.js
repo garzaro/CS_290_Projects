7d26ecbed58fdf4fb385504276b0faecc7504c55
garzaro <garzar@oregonstate.edu>
2017-08-05 10:41:30 -0500

    change port

--- Assignment_GET_POST_Checker/form_checker.js
+++ Assignment_GET_POST_Checker/form_checker.js
@@ -1,15 +1,15 @@
 var express = require('express');
 
 var app = express(); 
-var handlebars = require('express-handlebars').create({defaultLayout:'template'});
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 var bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 62521); 
+app.set('port', 61521); 
 
 app.get('/', function(req, res){
 	var qParams = []; 
@@ -17,33 +17,37 @@ app.get('/', function(req, res){
 		qParams.push({'name':p,'value':req.query[p]})
 	}
 	var context = {};
+	context.title = "GET request"; 
 	context.dataList = qParams; 
 	res.render('check', context); 
 });
 
-app.post('/showpost', function(req, res){
-
-	var qParams = []; 
-
-	for (var p in req.body){
-		qParams.push({'name':p, 'value':req.body[p]})
-	}
-	console.log(qParams);
-	console.log(req.body); 
-	var context = {}
-	context.dataList = qParams; 
-	res.render('check', context);
+app.post('/', function(req,res){
+	var qParams = [];
+  	for (var p in req.body){
+    	qParams.push({'name':p,'value':req.body[p]})
+ 	 }
+ 	 console.log(qParams);
+ 	 console.log(req.body);
+ 	 var context = {};
+ 	 context.title = "POST request"; 
+ 	 context.dataList = qParams;
+ 	 res.render('check', context);
 });
 
+
 app.use(function(req,res){
-	res.status(404); 
-	res.render('404');
+  	res.status(404);
+  	res.render('404');
 });
 
 app.use(function(err, req, res, next){
-	console.error(err.stack);
-	res.type('plain/text');
-	res.status(500); 
-	res.render('500');
+  	console.error(err.stack);
+  	res.type('plain/text');
+  	res.status(500);
+ 	 res.render('500');
 });
 
+app.listen(app.get('port'), function(){
+  	console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
